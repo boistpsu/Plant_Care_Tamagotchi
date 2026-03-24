a890ac6b27f7d1e8a83939d73b6f14ff4185c6cd
bazonkey <144395686+bazonkey@users.noreply.github.com>
2026-03-23 23:06:20 -0400

    3/23

--- lcd_output.py
+++ lcd_output.py
@@ -1,25 +1,31 @@
 import os, sys
-
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "Resources", "drivers"))
 from lib import LCD_2inch
 from PIL import Image
 import pygame as pg
+import threading
 
 disp = LCD_2inch.LCD_2inch()
 disp.Init()
 disp.clear()
 
-lcd_frame = 0
+_latest_image = None
+_lock = threading.Lock()
 
+def _lcd_worker():
+    while True:
+        with _lock:
+            img = _latest_image
+        if img is not None:
+            disp.ShowImage(img)
+
+thread = threading.Thread(target=_lcd_worker, daemon=True)
+thread.start()
 
 def send_to_lcd(surface):
-    global lcd_frame
-    lcd_frame += 1
-    if lcd_frame < 10:
-        return
-    lcd_frame = 0
-
+    global _latest_image
     raw = pg.image.tostring(surface, 'RGB')
     img = Image.frombytes('RGB', (320, 240), raw)
     img = img.rotate(90, expand=True)
-    disp.ShowImage(img)
+    with _lock:
+        _latest_image = img
