9b8e6e56bad04b38c73aaa6dcc8a6a4299f1227c
bazonkey <144395686+bazonkey@users.noreply.github.com>
2026-03-23 22:13:08 -0400

    3/23

---
+++ test.py
@@ -0,0 +1,12 @@
+import os, sys
+sys.path.append(os.path.expanduser('~/LCD_Module_RPI_code/RaspberryPi/python/lib'))
+import LCD_2inch
+from PIL import Image, ImageDraw
+
+disp = LCD_2inch.LCD_2inch()
+disp.Init()
+disp.clear()
+
+# draw a simple red rectangle
+img = Image.new('RGB', (320, 240), (255, 0, 0))
+disp.ShowImage(img)
