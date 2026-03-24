fb9df3d6e1fcd9276b96082ca8268a2ef2e1ad74
bazonkey <144395686+bazonkey@users.noreply.github.com>
2026-03-23 23:08:13 -0400

    3/23

--- lcd_output.py
+++ lcd_output.py
@@ -4,6 +4,7 @@ from lib import LCD_2inch
 from PIL import Image
 import pygame as pg
 import threading
+import time
 
 disp = LCD_2inch.LCD_2inch()
 disp.Init()
@@ -11,19 +12,26 @@ disp.clear()
 
 _latest_image = None
 _lock = threading.Lock()
+_busy = False
 
 def _lcd_worker():
+    global _busy
     while True:
+        time.sleep(0.1)  # only update LCD 10 times per second
         with _lock:
             img = _latest_image
         if img is not None:
+            _busy = True
             disp.ShowImage(img)
+            _busy = False
 
 thread = threading.Thread(target=_lcd_worker, daemon=True)
 thread.start()
 
 def send_to_lcd(surface):
     global _latest_image
+    if _busy:
+        return
     raw = pg.image.tostring(surface, 'RGB')
     img = Image.frombytes('RGB', (320, 240), raw)
     img = img.rotate(90, expand=True)
