a209c355e7acba0ac91c3e3b5b0b824250264bdb
bazonkey <144395686+bazonkey@users.noreply.github.com>
2026-03-31 20:27:02 -0400

    working button functionality

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from gpiozero import Button
 
 button = Button(18)
-
-while True:
-    if button.is_pressed:
-        print("The button was pressed!")
+print(button)
+button.wait_for_press()
+print("button was pressed")
+print(button)
